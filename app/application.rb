c52da148e628d668ad2d6c75c0cfa3dcd844cf81
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 03:27:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item_found = @@items.find {|item| item.name == item_req}
       item_found.price unless nil
     else 
-      resp.status = 400
+      resp.status = 404
       resp.write "Route not found"
     end
     resp.finish
