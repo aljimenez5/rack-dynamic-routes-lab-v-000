23739c6e9d77dd769d33c903a4bc3b5c67a1591b
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 03:25:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_req = req.path.split("/items/").last
-      item = @@items.find {|item| item.name == item_req}
+      item_found = @@items.find {|item| item.name == item_req}
       item.price
     else 
       resp.status = 400
