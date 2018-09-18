193094aeea0b3ead97bb1f37f0c214d46a640270
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 03:20:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      price = @@item.find {|item| (item)
+      price = @@items.find {|item| (item)
     
     
   end
