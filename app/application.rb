8f05ba84aaf1d0ace587ac67600b87462ce7319c
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 03:26:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_req = req.path.split("/items/").last
+      item_req = req.path.split('/items/').last
       item_found = @@items.find {|item| item.name == item_req}
       item_found.price
     else 
